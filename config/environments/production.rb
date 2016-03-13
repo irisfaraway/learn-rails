78abad2a9393ddc19d194fc3b25fe72a577422f6
irisfaraway <iris.faraway@gmail.com>
2016-03-13 12:27:14 +0000

    set up minitest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,4 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-
-  # Let me use the console
-  config.web_console.development_only = false
 end
